30182baf40bf7da9bc0b2e1ac107cde2cf952468
yoezf <95969630+yoezf@users.noreply.github.com>
2024-12-24 03:44:51 +0700

    Delete public directory

--- public/src/dist/js/script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle("is-active");
-
-    menu.classList.toggle("menu-active");
-});
